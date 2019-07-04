ecd5ed2a5ad6e80f67ae96e6c0ec654f432cca9f
Tim Ruffles <timruffles@gmail.com>
2019-07-04 17:45:44 +0100

    ugh x-platform with posix tools just ain't work it

---
+++ script/functional-test.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+require 'minitest/autorun'
+
+
+describe "functional test" do
+  before do
+    FileUtils.mkdir_p(".github")
+
+    File.write(".github/main.workflow", %q(
+workflow "workflow one" {
+  on = "push"
+  resolves = [
+    "action one",
+  ]
+}
+
+workflow "per minute" {
+  on = "schedule(* * * * *)"
+  resolves = [
+    "action one",
+  ]
+}
+
+action "action one" {
+  uses = "docker://alpine"
+  runs = ["sh", "-c", "echo $GITHUB_SHA"]
+}
+))
+
+    `./bin/migrate-actions`
+  end
+
+
+  it "outputs expected files" do
+    actual = Dir[".github/workflows/*.yml"]
+    assert_equal actual, [".github/workflows/schedule.yml", ".github/workflows/push.yml"]
+  end
+
+  it "handles push correctly" do
+    push = File.read(".github/workflows/push.yml")
+
+    assert_equal %q(on: push
+name: workflow one
+jobs:
+  actionOne:
+    name: action one
+    steps:
+    - name: action one
+      uses: docker://alpine
+      entrypoint: sh -c echo $GITHUB_SHA
+), push
+  end
+
+  it "handles schedule correctly" do
+    schedule = File.read(".github/workflows/schedule.yml")
+
+    assert_equal %q(on:
+  schedules:
+  - cron: '* * * * *'
+name: per minute
+jobs:
+  actionOne:
+    name: action one
+    steps:
+    - name: action one
+      uses: docker://alpine
+      entrypoint: sh -c echo $GITHUB_SHA
+), schedule
+  end
+end
