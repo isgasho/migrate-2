8a8ada297c96caf6c81e66367e5fba93ffb0261d
Tim Ruffles <timruffles@gmail.com>
2019-07-18 10:25:28 +0100

    Fixtures and README

--- README.md
+++ README.md
@@ -7,3 +7,52 @@ This tool will generate a set of GitHub Actions YAML workflow files from your `.
 The original beta version of Actions supported parallel Action execution while sharing a workspace. In V2, we do support parallel execution
 of jobs, but jobs do not share workspaces.
 
+## Install
+
+Head over to the [releases](https://github.com/actions/migrate/releases) tab, and download the executable for your operating system.
+
+Once you've downloaded it, navigate to a repository using Actions V1 and run the `migrate-actions` executable.
+
+```sh
+> cd path/to/your/repo
+> cat ./.github/main.workflow
+workflow "on push" {
+    on = "push"
+    resolves = ["say hi"]
+}
+workflow "on pull request" {
+    on = "pull_request"
+    resolves = ["say hi"]
+}
+action "say hi" {
+    uses = "docker/whalesay@master"
+    runs = "whalesay hello actions"
+}
+> path/to/migrate
+Created workflow .github/workflows/push.yml
+Created workflow .github/workflows/pull_request.yml
+> tail -n +1 .github/workflows/*.yml
+==> .github/workflows/pull_request.yml <==
+on: pull_request
+name: on pull request
+jobs:
+  sayHi:
+    name: say hi
+    steps:
+    - name: say hi
+      uses: docker/whalesay@master
+      entrypoint: whalesay hello actions
+
+==> .github/workflows/push.yml <==
+on: push
+name: on push
+jobs:
+  sayHi:
+    name: say hi
+    steps:
+    - name: say hi
+      uses: docker/whalesay@master
+      entrypoint: whalesay hello actions
+```
+
+You can then delete your `main.workflow`. If you have any `.yml` files in `.github/workflows` your `main.workflow` will be ignored.
--- commands/root.go
+++ commands/root.go
@@ -54,9 +54,25 @@ var rootCmd = &cobra.Command{
 			return
 		}
 
+		if len(files) == 0 {
+			failed("Could not find any workflows to convert")
+			return
+		}
+
 		for _, file := range files {
 			writeFile(file)
+			fmt.Printf("Created workflow %s\n", file.Path)
 		}
+
+		fmt.Println("")
+		fmt.Println("You can now delete your main.workflow file. If you have any .yml files in .github/workflows your main.workflow will be ignored.")
+		fmt.Println("    rm .github/main.workflow")
+		fmt.Println("    git add -A .github/main.workflow .github/workflows/*.yml")
+		fmt.Println("    git commit -m 'converted main.workflow to Actions V2 yml files'")
+		fmt.Println("")
+
+		fmt.Println("Thanks for being a")
+		fmt.Println(banner)
 	},
 }
 
@@ -93,3 +109,16 @@ func exitWithMessage(msg string) {
 	os.Exit(1)
 
 }
+
+const banner = `  ________.__  __     ___ ___      ___.        _____          __  .__                      
+ /  _____/|__|/  |_  /   |   \ __ _\_ |__     /  _  \   _____/  |_|__| ____   ____   ______
+/   \  ___|  \   __\/    ~    \  |  \ __ \   /  /_\  \_/ ___\   __\  |/  _ \ /    \ /  ___/
+\    \_\  \  ||  |  \    Y    /  |  / \_\ \ /    |    \  \___|  | |  (  <_> )   |  \\___ \ 
+ \______  /__||__|   \___|_  /|____/|___  / \____|__  /\___  >__| |__|\____/|___|  /____  >
+        \/                 \/           \/          \/     \/                    \/     \/ 
+__________        __           ___________              __                                 
+\______   \ _____/  |______    \__    ___/___   _______/  |_  ___________                  
+ |    |  _// __ \   __\__  \     |    |_/ __ \ /  ___/\   __\/ __ \_  __ \                 
+ |    |   \  ___/|  |  / __ \_   |    |\  ___/ \___ \  |  | \  ___/|  | \/                 
+ |______  /\___  >__| (____  /   |____| \___  >____  > |__|  \___  >__|                    
+        \/     \/          \/               \/     \/            \/        `
